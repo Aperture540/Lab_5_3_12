3f9513c60ae9712e53b2c2087b6750b93fdf744f
Anthony Gendron <aperturescience540@gmail.com>
2017-10-09 13:50:09 -0400

    Lab_5_3_12

--- main.cpp
+++ main.cpp
@@ -16,108 +16,86 @@
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-    
+class FarmAnimal 
+{
+public:
+    FarmAnimal(double water_consumption);
+    double getWaterConsumption();
+    // ...
+private:
+    double water_consumption;
+};
+
+FarmAnimal::FarmAnimal(double water_consumption) 
+{
+    this->water_consumption = water_consumption;
+}
+
+double FarmAnimal::getWaterConsumption() 
+{
+    return water_consumption;
+}
+
+class ConsumptionAccumulator 
+{
+public:
+    ConsumptionAccumulator();
+    double getTotalConsumption();
+    void addConsumption(FarmAnimal animal);
+private:
+    double total_consumption;
+};
+
+ConsumptionAccumulator::ConsumptionAccumulator() :
+total_consumption(0) 
+{
+}
+
+double ConsumptionAccumulator::getTotalConsumption() 
+{
+    return total_consumption;
+}
+
+void ConsumptionAccumulator::addConsumption(FarmAnimal animal) 
+{
+    total_consumption += animal.getWaterConsumption();
+}
+
+int main() 
+{
+    ConsumptionAccumulator accumulator;
+
+    // read user input
+    // create appropriate objects and add them to the accumulator
+
     double myNumber = 0;
     double total = 0;
     string input;
-    
-    do
+
+    do 
     {
         getline(cin, input);
-        if( 1 == sscanf(input.c_str(), "cow %lf", &myNumber))
+        if (1 == sscanf(input.c_str(), "cow %lf", &myNumber)) 
         {
-            myNumber = (myNumber/100)*8.6;
-            total += myNumber;
-            //cout << myNumber << endl;
-            //cout << "---------" << endl;
-        }
-        else if ( 1 == sscanf(input.c_str(), "sheep %lf", &myNumber))
+            myNumber = (myNumber / 100)*8.6;
+            accumulator.addConsumption(myNumber);
+        } 
+        else if (1 == sscanf(input.c_str(), "sheep %lf", &myNumber)) 
         {
-            myNumber = (myNumber/10)*1.1;
-            total += myNumber;
-            //cout << myNumber << endl;
-            //cout << "---------" << endl;
-        }
-        else if ( 1 == sscanf(input.c_str(), "horse %lf", &myNumber))
+            myNumber = (myNumber / 10)*1.1;
+            accumulator.addConsumption(myNumber);
+        } 
+        else if (1 == sscanf(input.c_str(), "horse %lf", &myNumber)) 
         {
-            myNumber = (myNumber/100)*6.8;
-            total += myNumber;
-            //cout << myNumber << endl;
-            //cout << "---------" << endl;
+            myNumber = (myNumber / 100)*6.8;
+            accumulator.addConsumption(myNumber);
         }
-        else
-        {
-            //cout << "No Match!" << endl;
-            cout << total << endl;
-            //cout << "---------" << endl;
-        }
-    }
+    }    
     while (input != "");
-     
-     return 0;
+
+    cout << accumulator.getTotalConsumption();
+
+    return 0;
 }
-     
-    
-/*
-    class FarmAnimal{ 
-    public:
-        FarmAnimal(double water_consumption); 
-        double getWaterConsumption();
-        // ...
-    private:
-        double water_consumption;
-    };
-    
-    FarmAnimal::FarmAnimal(double water_consumption) { 
-        this->water_consumption = water_consumption;
-    }
-    
-    double FarmAnimal::getWaterConsumption() { 
-        return water_consumption;
-    }
-    
-    class ConsumptionAccumulator 
-    {
-    public:
-        ConsumptionAccumulator();
-        double getTotalConsumption();
-        //void addConsumption(Animal animal);
-    private:
-        double total_consumption;
-    };
-    
-    ConsumptionAccumulator::ConsumptionAccumulator() : 
-        total_consumption(0)
-    { 
-    }
-    
-    double ConsumptionAccumulator::getTotalConsumption() 
-    {
-        return total_consumption; 
-    }
-    
-    /*void ConsumptionAccumulator::addConsumption(Animal animal) 
-    {
-        total_consumption += animal.getWaterConsumption(); 
-    }*/
-    
-/*    
-    int main() 
-    {
-        ConsumptionAccumulator accumulator;
-        
-        // read user input
-        // create appropriate objects and add them to the accumulator
-        
-        
-        
-        cout << accumulator.getTotalConsumption(); 
-        
-        return 0;
-    }
-*/
+
 
